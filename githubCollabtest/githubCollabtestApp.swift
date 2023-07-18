a69f4e6a735532e3e474fc99ab0a4315256d56a9
emily <eli.mhs24@gmail.com>
2023-07-18 09:54:55 -0500

    Initial Commit

---
+++ githubCollabtest/githubCollabtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubCollabtestApp.swift
+//  githubCollabtest
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct githubCollabtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
